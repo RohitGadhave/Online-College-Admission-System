578d5d321a824f9596e054abf3d5f30b26ee3dda
RohitGadhave <rohitgadhave249@gmail.com>
2020-06-18 15:24:17 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,57 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Welcome Home</title>
+
+<link rel="stylesheet" href="css/style.css" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body, html {
+    height: 100%;
+    margin: 0;
+}
+
+.bg {
+    /* The image used */
+    background-image: url("Latvia-University-of-Jelgava.jpg");
+
+    /* Full height */
+    height: 100%; 
+
+    /* Center and scale the image nicely */
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+.button {
+    background-color: #555555;
+    border: none;
+    color: white;
+    padding: 10px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+}
+
+
+
+</style>
+</head>
+<body bgcolor="#E6E6FA" class="bg">
+	<p align="right"><a class="button" href="login.php">Login</a><br>
+	<a class="button" href="registration.php">Register Here</a><br>
+	<a class="button" href="admin.php">Admin</a></p>
+
+<div class="form">
+<h1><font color="blue">University of Internet Education</h1>
+
+
+</div>
+</body>
+</html>
